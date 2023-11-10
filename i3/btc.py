3b7cd99477b187ac0b1bbb942104aeda559732d6
Eduardo Ferreira <eduardo.ferreira@rockridge.io>
2023-11-10 14:42:23 +0000

    Redo how btc is done

--- i3/btc.py
+++ i3/btc.py
@@ -1,12 +1,43 @@
+"""BTC"""
+import argparse
 import json
+
 import requests
-import fileinput
-
-btc = json.loads(requests.get('https://api.coindesk.com/v1/bpi/currentprice.json').content)
 
 
-print("BTC: <${:.2f} | €{:.2f}>".format(
-    float(btc['bpi']['USD']['rate'].replace(',', '')),
-    float(btc['bpi']['EUR']['rate'].replace(',', ''))))
+def main(check: bool) -> None:
+    """Main."""
+    if check:
+        with open('/home/eduardo/btc_price.json', 'r', encoding='utf8') as f_json:
+            btc = json.loads(
+                requests.get(
+                    'https://api.coindesk.com/v1/bpi/currentprice.json',
+                    timeout=30).content)
+            j_file = json.load(f_json)
+
+        j_file["BTC"]["EUR"] = btc['bpi']['EUR']['rate_float']
+        j_file["BTC"]["USD"] = btc['bpi']['USD']['rate_float']
+
+        with open('/home/eduardo/btc_price.json', 'w', encoding='utf8') as f_json:
+            f_json.write(json.dumps(j_file))
+
+    else:
+        with open('/home/eduardo/btc_price.json', 'r', encoding='utf8') as f_json:
+            j_file = json.load(f_json)
+            print(
+                f"BTC: <${j_file['BTC']['USD']:.2f} | €{j_file['BTC']['EUR']:.2f}>")
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--check",
+        action="store_true",
+        help="Update the price",
+        required=False)
+
+    args = parser.parse_args()
+
+    main(args.check)
+
+# vim: tw=100 colorcolumn=100
